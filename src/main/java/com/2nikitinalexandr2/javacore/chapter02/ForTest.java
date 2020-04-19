b08de415446c90f642302d0bbced1e664ad0756b
Sasha Nikitin <2nikitinalexandr2@gmail.com>
2020-04-20 03:53:33 +0500

    Adding new classes in packages chapter02 and chapter03

--- src/main/java/com/2nikitinalexandr2/javacore/chapter02/ForTest.java
+++ src/main/java/com/2nikitinalexandr2/javacore/chapter02/ForTest.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+/*
+    Продемонстрировать применение цикоа for.
 
+    Присвоить исходному файлу имя "ForTest.java"
+ */
 public class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        for(x = 0; x<10; x++)
+            System.out.println("Значение x:" + x);
+    }
 }
+
--- src/main/java/com/2nikitinalexandr2/javacore/chapter03/BoolTest.java
+++ src/main/java/com/2nikitinalexandr2/javacore/chapter03/BoolTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
+// Продемонстрировать применение значений типа boolean
 public class BoolTest {
+    public static void main(String[] args) {
+        boolean b;
+
+        b = false;
+        System.out.println("b равно " + b);
+        b = true;
+        System.out.println("b равно " + b);
+
+        // значение типа boolean может управлять оператором if
+        if(b) System.out.println("Этот код выролняется.");
+        b = false;
+        if (b) System.out.println("Этот код не выполняется");
+
+        // результат сравнения - значение типа boolean
+        System.out.println("10 > 9 равно " + (10 > 9));
+    }
 }
