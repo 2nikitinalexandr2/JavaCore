44ffac3a763a5d7b457898fe668a5c5140b13ba9
Sasha Nikitin <2nikitinalexandr2@gmail.com>
2020-05-22 02:18:19 +0500

    Adding some clases(DoWhile.java,Menu.java,StringSwitch.jav) in package chapter05

---
+++ src/main/java/com/2nikitinalexandr2/javacore/chapter05/DoWhile.java
@@ -0,0 +1,10 @@
+// Продемонстрировать применение оператора цикла do-while
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+
+        do {
+            System.out.println("такт " + n);
+        } while (--n > 0);
+    }
+}
--- src/main/java/com/2nikitinalexandr2/javacore/chapter05/StringSwitch.java
+++ src/main/java/com/2nikitinalexandr2/javacore/chapter05/StringSwitch.java
@@ -17,4 +17,4 @@ public class StringSwitch {
                 System.out.println("не совпало");
         }
     }
-}WS
+}
