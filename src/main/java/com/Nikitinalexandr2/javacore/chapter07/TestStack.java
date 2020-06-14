221bd5d2197c6aaa3285da171a803f8bcfbc7958
Sasha Nikitin <2nikitinalexandr2@mgil.com>
2020-06-15 04:20:57 +0500

    adding and update classes and package's name

--- src/main/java/com/Nikitinalexandr2/javacore/chapter07/TestStack.java
+++ src/main/java/com/Nikitinalexandr2/javacore/chapter07/TestStack.java
@@ -34,7 +34,7 @@ class Stack {
             return stck[tos--];
     }
 }
-public class TestStack {
+class TestStack {
     public static void main(String[] args) {
         Stack mystack1 = new Stack();
         Stack mystack2 = new Stack();
